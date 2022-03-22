4b48d5bd6e286bf77285456eed016c283559e9af
HoooDyyy <101186790+HoooDyyy@users.noreply.github.com>
2022-03-22 11:34:01 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+     int i =1;
+  char c = 'c';
+  double d = 34;
+  float e = 345;
+    System.out.println(""+i);
+    System.out.println(""+c);
+    System.out.println(""+d);
+    System.out.println(""+e);
+  
+  } 
+}
